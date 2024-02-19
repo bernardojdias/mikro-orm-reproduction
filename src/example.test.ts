b3eb1b964991aac31d91ebe083b685d2581a1e78
Bernardo Dias <bernardo.dias@beltraocoelho.pt>
2024-02-19 16:29:37 +0000

    change to meet reproduction requirements

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,51 +1,147 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import path from "path";
+import { EntityGenerator } from "@mikro-orm/entity-generator";
+import { Migrator } from "@mikro-orm/migrations";
+import {
+	Entity,
+	MikroORM,
+	Opt,
+	Options,
+	PrimaryKey,
+	Property,
+} from "@mikro-orm/mysql";
+import dotenv from "dotenv";
+import fs from "fs/promises";
+
+dotenv.config({ path: path.join(__dirname, "../.env") });
 
 @Entity()
-class User {
+export class Entity1 {
+	@PrimaryKey()
+	id!: number;
 
-  @PrimaryKey()
-  id!: number;
+	@Property({ length: 150, default: "0" })
+	name!: string & Opt;
 
-  @Property()
-  name: string;
+	@Property({ default: 0 })
+	aNumber!: number & Opt;
 
-  @Property({ unique: true })
-  email: string;
+	@Property({ length: 0, defaultRaw: "CURRENT_TIMESTAMP" })
+	dateCreate!: Date & Opt;
 
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
-  }
-
+	@Property({ length: 0, defaultRaw: "CURRENT_TIMESTAMP" })
+	dateUpdate!: Date & Opt;
 }
 
-let orm: MikroORM;
+@Entity()
+export class Entity2 {
+	@PrimaryKey()
+	id!: number;
+
+	@Property({ length: 150, default: "0" })
+	name!: string;
+
+	@Property({ default: 0 })
+	aNumber!: number;
+
+	@Property({ defaultRaw: "CURRENT_TIMESTAMP" })
+	dateCreate!: Date;
+
+	@Property({ defaultRaw: "CURRENT_TIMESTAMP" })
+	dateUpdate!: Date;
+}
+
+const commonOptions = {
+	dbName: process.env.DB_NAME,
+	port: parseInt(process.env.DB_PORT ?? "", 10),
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	password: process.env.DB_PASSWORD,
+	extensions: [Migrator, EntityGenerator],
+	debug: false,
+	allowGlobalContext: true, // only for testing
+} satisfies Options;
 
 beforeAll(async () => {
-  orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
-    debug: ['query', 'query-params'],
-    allowGlobalContext: true, // only for testing
-  });
-  await orm.schema.refreshDatabase();
+	const orm = await MikroORM.init({
+		...commonOptions,
+		entities: [],
+		discovery: {
+			warnWhenNoEntities: false,
+		},
+	});
+
+	await orm.schema.refreshDatabase();
 });
 
 afterAll(async () => {
-  await orm.close(true);
+	const orm = await MikroORM.init({
+		...commonOptions,
+		entities: [],
+		discovery: {
+			warnWhenNoEntities: false,
+		},
+	});
+
+	const migrator = orm.getMigrator();
+	await migrator.down();
+	await orm.close(true);
+
+	await fs.rmdir(path.join(__dirname, "./migrations"), { recursive: true });
+	await fs.rmdir(path.join(__dirname, "./error-migrations"), {
+		recursive: true,
+	});
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
+test("Migration: create table with date field as varchar", async () => {
+	const orm = await MikroORM.init({
+		...commonOptions,
+		entities: [Entity1],
+		migrations: {
+			path: path.join(__dirname, "./error-migrations"),
+		},
+	});
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+	const migrator = orm.getMigrator();
+	const migration = await migrator.createMigration();
+	const { up } = migration.diff;
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+	const isDateCreateVarchar = up.some((s) =>
+		s.includes("`date_create` varchar"),
+	);
+
+	await orm.close(true);
+
+	expect(isDateCreateVarchar).toBe(true);
+});
+
+test("Migration: create table with date field as datetime", async () => {
+	const orm = await MikroORM.init({
+		...commonOptions,
+		entities: [Entity2],
+	});
+
+	const migrator = orm.getMigrator();
+	const migration = await migrator.createMigration();
+	const { up } = migration.diff;
+
+	const isDateCreateDateTime = up.some((s) =>
+		s.includes("`date_create` datetime"),
+	);
+
+	await migrator.up();
+	await orm.close(true);
+
+	expect(isDateCreateDateTime).toBe(true);
+});
+
+test("EntityGenerator: dateCreate with Opt type", async () => {
+	const orm = await MikroORM.init({
+		...commonOptions,
+		entities: [Entity2],
+	});
+
+	const generator = orm.getEntityGenerator();
+	const result = await generator.generate();
+
+	expect(result[0].includes("dateCreate!: Date & Opt;")).toBe(true);
 });
